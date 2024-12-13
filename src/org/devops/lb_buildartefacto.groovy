12dcfd315ead41da160251353a5e9aab7415b8ab
andreslugo <andreslugo@periferia-it.com>
2024-12-13 17:27:35 -0500

    Intento 39

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -1,12 +1,11 @@
 package org.devops
 
-def clone() {
-    echo "Iniciando clonación del repositorio: ${env.GIT_URL_1}, rama: ${env.GIT_BRANCH_1}"
-    git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
-}
+class lb_buildartefacto {
+    static def clone() {
+        git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
+    }
 
-def install() {
-    echo "Ejecutando npm install en el directorio actual:"
-    sh 'ls -l' // Muestra los archivos presentes para verificar si está package.json
-    sh 'npm install'
+    static def install() {
+        sh 'npm install'
+    }
 }
