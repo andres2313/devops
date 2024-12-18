39aad05c924c9c15271162929155417074f7c2d3
andreslugo <andreslugo@periferia-it.com>
2024-12-18 16:34:24 -0500

    Cambios 10

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -3,7 +3,7 @@ package org.devops
 def despliegueContenedor(projectGitName){
     //sh "docker stop ${projectGitName}"
     //sh "docker rm ${projectGitName}"
-    sh "docker pull andres2313/Jenkisfile"
+    sh "docker pull andreslugo/react-test-jenkisfile"
     sh """ docker run -d --name ${projectGitName} \
     --network=${env.NameNetwork} -p 5174:5174 \
     --user root andres2313/${projectGitName}
