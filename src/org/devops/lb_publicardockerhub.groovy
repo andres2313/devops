3ad0ee449e83dad46b196cbc35f6f8eb9faa690a
andreslugo <andreslugo@periferia-it.com>
2024-12-19 12:37:40 -0500

    cambio 21

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -4,9 +4,21 @@ def publicarImage(projectGitName){
     withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}",
     passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         sh "set -x"
-        sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-        sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest"
-        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest"
+        sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
+        sh "docker tag andreslugo/devops andreslugo/devops:latest"
+        sh "docker push andreslugo/devops:latest"
         sh "docker images"  // Para verificar las imágenes locales disponibles.
     }
 }
+
+//withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+           // script {
+                // Login en Docker Hub
+             //   sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
+                
+                // Etiquetar la imagen correctamente
+               // sh "docker tag andreslugo/devops andreslugo/devops:latest"
+                
+                // Publicar la imagen a Docker Hub
+                //sh "docker push andreslugo/devops:latest"
+           // }
