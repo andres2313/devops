20904a1f1797e9ee85553290439f5e00562191c5
andreslugo <andreslugo@periferia-it.com>
2024-12-19 12:17:38 -0500

    cambio 18

--- src/org/devops/lb_buildimagen.groovy
+++ src/org/devops/lb_buildimagen.groovy
@@ -1,5 +1,6 @@
 package org.devops
 
 def buildImageDocker(projectGitName){
+    sh "docker tag andreslugo/${projectGitName}, andreslugo/${projectGitName} ."
     sh "docker build -t andreslugo/${projectGitName} ." 
 }
