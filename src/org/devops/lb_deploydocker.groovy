8e878714cff9597fc7fe69937d88e843c1008aaf
andreslugo <andreslugo@periferia-it.com>
2024-12-19 12:40:48 -0500

    cambio 23

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -6,11 +6,11 @@ def despliegueContenedor(projectGitName){
     //sh "docker rm ${projectGitName}"
     
     // Pull de la imagen más reciente desde Docker Hub
-    sh "docker pull ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest"
+    sh "docker pull ${DOCKERHUB_USERNAME}/${projectGitName}:latest"
 
     // Desplegar el contenedor en la red y puerto definidos
     sh """ docker run -d --name ${projectGitName} \
     --network=${env.NameNetwork} -p 5174:5174 \
-    --user root ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest
+    --user root ${DOCKERHUB_USERNAME}/${projectGitName}:latest
     """
 }
