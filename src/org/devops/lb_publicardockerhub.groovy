26843a67428734f8adc690501926a47e4209735a
andreslugo <andreslugo@periferia-it.com>
2024-12-19 12:46:03 -0500

    cambio 25

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -4,7 +4,7 @@ def publicarImage(projectGitName){
     withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}",
     passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         sh "set -x"
-        sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
+        sh "docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD"
         sh "docker tag andreslugo/devops andreslugo/devops:latest"
         sh "docker push andreslugo/devops:latest"
         sh "docker images"  // Para verificar las imágenes locales disponibles.
