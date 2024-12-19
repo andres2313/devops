dc5a31a2a875064f15c0166617d7bc08e55e260c
andreslugo <andreslugo@periferia-it.com>
2024-12-19 17:26:01 -0500

    cambio 37

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -5,7 +5,7 @@ def analisisOwasp(projectGitName){
     sh """ docker run --rm -v /path/to/local/ProjectOwasp:/zap/wrk/:rw \
           --user root --network=${env.NameNetwork} \
           -t edansama96/zap2docker-stable \
-           zap-full-scan.py \
+           zap-webswing.sh \
           -t ${env.dominio} \
           -r /zap/wrk/ProjectOwasp.html -I
        """
