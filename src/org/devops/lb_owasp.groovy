c85106a6e74f762b0605865b921192d873b72f16
andreslugo <andreslugo@periferia-it.com>
2024-12-19 18:45:48 -0500

    cambio 42

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -1,22 +1,22 @@
 package org.devops
 
-def analisisOwasp(projectGitName){
-    //Ejecutar el análisis OWASP con ZAP, asegurando volumen y variables correctas
-    sh """ docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
-          --user root --network=${env.NameNetwork} \
-          -t edansama96/zap2docker-stable \
-           zap-webswing.sh \
-          -t ${env.dominio} \
-          -r ProjectOwasp.html -I
-       """
-}
 //def analisisOwasp(projectGitName){
-//sh """ docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
-//       --user root --network=${env.NameNetwork} \
-//       -t owasp/zap2docker-stable \
-//       zap-full-scan.py \
-//       -t ${env.dominio} \
-//       -r ProjectOwasp.html -I
-//       """ 
+    //Ejecutar el análisis OWASP con ZAP, asegurando volumen y variables correctas
+  //  sh """ docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
+  //        --user root --network=${env.NameNetwork} \
+  //        -t edansama96/zap2docker-stable \
+  //         zap-webswing.sh \
+  //        -t ${env.dominio} \
+  //        -r ProjectOwasp.html -I
+  //     """
 //}
+def analisisOwasp(projectGitName){
+sh """ docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
+       --user root --network=${env.NameNetwork} \
+       -t owasp/zap2docker-stable \
+       zap-full-scan.py \
+       -t ${env.dominio} \
+       -r ProjectOwasp.html -I
+       """ 
+}
 
