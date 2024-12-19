78dd4828b84df4ee724eac98c931e98c09882a3e
andreslugo <andreslugo@periferia-it.com>
2024-12-19 18:03:39 -0500

    cambio 40

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -2,12 +2,12 @@ package org.devops
 
 def analisisOwasp(projectGitName){
     //Ejecutar el análisis OWASP con ZAP, asegurando volumen y variables correctas
-    sh """ docker run --rm -v /path/to/local/ProjectOwasp:/zap/wrk/:rw \
+    sh """ docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
           --user root --network=${env.NameNetwork} \
           -t edansama96/zap2docker-stable \
            zap-webswing.sh \
           -t ${env.dominio} \
-          -r /zap/wrk/ProjectOwasp.html -I
+          -r /zap/ProjectOwasp.html -I
        """
 }
 //def analisisOwasp(projectGitName){
